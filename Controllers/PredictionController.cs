6956dbef5a1115c87b17d30da34755b8552aa832
deedeemaan <denisalorincz22@yahoo.com>
2026-01-27 21:51:20 +0200

    lab9

--- Controllers/PredictionController.cs
+++ Controllers/PredictionController.cs
@@ -53,4 +53,60 @@ public class PredictionController : Controller
         var result = await query.ToListAsync();
         return View(result);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Dashboard(DateTime? fromDate, DateTime? toDate)
+    {
+        var query = _context.PredictionHistories.AsQueryable();
+
+        if (fromDate.HasValue)
+            query = query.Where(p => p.CreatedAt.Date >= fromDate.Value.Date);
+
+        if (toDate.HasValue)
+            query = query.Where(p => p.CreatedAt.Date <= toDate.Value.Date);
+
+        var totalPredictions = await query.CountAsync();
+
+        var paymentTypeStats = await query
+            .GroupBy(p => p.PaymentType)
+            .Select(g => new PaymentTypeStat
+            {
+                PaymentType = g.Key,
+                AveragePrice = g.Average(x => x.PredictedPrice),
+                Count = g.Count()
+            })
+            .ToListAsync();
+
+        var prices = await query.Select(p => p.PredictedPrice).ToListAsync();
+
+        var buckets = new List<PriceBucketStat>
+    {
+        new PriceBucketStat { Label = "0 - 10" },
+        new PriceBucketStat { Label = "10 - 20" },
+        new PriceBucketStat { Label = "20 - 30" },
+        new PriceBucketStat { Label = "30 - 50" },
+        new PriceBucketStat { Label = "> 50" }
+    };
+
+        foreach (var price in prices)
+        {
+            if (price < 10) buckets[0].Count++;
+            else if (price < 20) buckets[1].Count++;
+            else if (price < 30) buckets[2].Count++;
+            else if (price < 50) buckets[3].Count++;
+            else buckets[4].Count++;
+        }
+
+        var vm = new DashboardViewModel
+        {
+            TotalPredictions = totalPredictions,
+            PaymentTypeStats = paymentTypeStats,
+            PriceBuckets = buckets,
+            FromDate = fromDate,
+            ToDate = toDate
+        };
+
+        return View(vm);
+    }
+
 }
---
+++ Models/PriceBucketStat.cs
@@ -0,0 +1,5 @@
+﻿public class PriceBucketStat
+{
+    public string Label { get; set; } = string.Empty;
+    public int Count { get; set; }
+}
